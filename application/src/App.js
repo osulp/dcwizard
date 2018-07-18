200563480fccd420f2af16fa64d708825123411a
Sam Young <sammysamx20@gmail.com>
2018-07-18 16:23:44 -0700

    Main applicatio reads from the JSON now. States are glitchy now so user inputs aren't saved

--- application/src/App.js
+++ application/src/App.js
@@ -8,6 +8,8 @@ import {ModalHeader} from 'reactstrap';
 import {ModalBody} from 'reactstrap';
 import {ModalFooter} from 'reactstrap';
 import {StepWizard, Step} from 'react-step-wizard';
+import $ from 'jquery';
+import data from './me_with_others_data.json';
 import Home from "./Home";
 import Q1_2 from "./Q1_2";
 import Done1 from "./Done1";
@@ -22,16 +24,26 @@ import Q1_2_1_1 from "./Q1_2_1_1";
 import Q1_2_1_1_yes from "./Q1_2_1_1_yes";
 import Q1_2_1_1_no from "./Q1_2_1_1_no";
 class App extends Component {
-  constructor () {
-      super();
-      this.state ={
-         qwe: false,
-         chosen: null
-  }
+  constructor (props) {
+      super(props);
+      this.state = {
+        modal: false,
+        chosen: null,
+        qwe: false,
+        questions:{ }
+      };
+
+      this.toggle = this.toggle.bind(this);
 this.button_chosen = this.button_chosen.bind(this);
 this.reset = this.reset.bind(this); // you are missing this line
 this.SetStart = this.SetStart.bind(this)
 this.select_question = this.select_question.bind(this);
+this.question_counter = this.question_counter.bind(this);
+  }
+  toggle() {
+    this.setState({
+      modal: !this.state.modal
+    });
   }
   SetStart = () => {
     this.setState({chosen: null});
@@ -62,134 +74,9 @@ this.select_question = this.select_question.bind(this);
   reset(){
     window.location.href="/";
   }
-Q1_1check(){
-  if (window.location.href.indexOf("Q1_1") != -1 || window.location.href.indexOf("Q1_2") != -1 ||window.location.href.indexOf("Q1_2_1") != -1 || window.location.href.indexOf("Done1") != -1){
-return(<div/>);
-  }
-  else{
-    return(
-  <li><NavLink to="/Q1_1"><Button >Start</Button></NavLink></li>
 
-);
-}
-}
-Q1_2check(){
-  {/*the extra if case is for making the button not appear... there has to be a better way of doign all of this */}
-  if (window.location.href.indexOf("Q1_1") > -1 && window.location.href.indexOf("Q1_1_2") == -1 ){
-return(<li><Link to="/Q1_2"><Button >Yes</Button></Link>
-        <Link to="/Q1_1_2"><Button >No</Button></Link></li>);
-  }
 
-  else{
-    return(
-<div/>
-);
-}
-}
-Q1_2_1check(){
-  if (window.location.href.indexOf("Q1_2") > -1 && window.location.href.indexOf("Q1_2_1") == -1 && window.location.href.indexOf("Q1_2_2") == -1 ){
-return(<li><Link to="/Q1_2_1"><Button >Yes</Button></Link>  <Link to="/Q1_2_2"><Button >No</Button></Link></li>);
-  }
 
-  else{
-    return(
-<div/>
-);
-}
-}
-Done1check(){
-  if (window.location.href.indexOf("Q1_2_1") > -1 && window.location.href.indexOf("Q1_2_1_1") == -1){
-return(<li><Link to="/Done1"><Button >Yes</Button></Link><Link to="/Q1_2_1_1"><Button >No</Button></Link></li>);
-  }
-
-  else{
-    return(
-<div/>
-);
-}
-}
-finalcheck1(){
-  if (window.location.href.indexOf("Done1") > -1){
-return(<li><Link to="/"><Button >Start Again</Button></Link></li>);
-  }
-
-  else{
-    return(
-
-<div/>
-);
-}
-}
-Q1_1_2check(){
-  if (window.location.href.indexOf("Q1_1_2") > -1 && window.location.href.indexOf("Q1_1_2_yes") == -1 && window.location.href.indexOf("Q1_1_2_no") == -1 ){
-return(<li><Link to="/Q1_1_2_yes"><Button >Yes</Button></Link>
-      <Link to="/Q1_1_2_no"><Button >No</Button></Link></li>);
-  }
-
-  else{
-    return(
-<div/>
-);
-}
-}
-Q1_1_2_yesnocheck(){
-  if (window.location.href.indexOf("Q1_1_2_yes") > -1 || window.location.href.indexOf("Q1_1_2_no") > -1){
-return(<li><Link to="/"><Button >Start Again</Button></Link></li>);
-  }
-
-  else{
-    return(
-<div/>
-);
-}
-}
-Q1_2_2check(){
-  if (window.location.href.indexOf("Q1_2_2") > -1 && window.location.href.indexOf("Q1_2_2_yes") == -1 && window.location.href.indexOf("Q1_2_2_no") == -1){
-return(<li><Link to="/Q1_2_2_yes"><Button >Yes</Button></Link>
-<Link to="/Q1_2_2_no"><Button >No</Button></Link></li>);
-  }
-
-  else{
-    return(
-<div/>
-);
-}
-}
-Q1_2_2_yesnocheck(){
-  if (window.location.href.indexOf("Q1_2_2_yes") > -1 || window.location.href.indexOf("Q1_2_2_no") > -1){
-return(<li><Link to="/"><Button >Start Again</Button></Link></li>);
-  }
-
-  else{
-    return(
-<div/>
-);
-}
-}
-
-Q1_2_1_1check(){
-  if (window.location.href.indexOf("Q1_2_1_1") > -1 && window.location.href.indexOf("Q1_2_1_1_yes") == -1 && window.location.href.indexOf("Q1_2_1_1_no") == -1){
-return(<li><Link to="/Q1_2_1_1_yes"><Button >Yes</Button></Link><Link to="/Q1_2_1_1_no"><Button >No</Button></Link></li>);
-  }
-
-  else{
-    return(
-<div/>
-);
-}
-}
-
-Q1_2_1_1yesnocheck(){
-  if (window.location.href.indexOf("Q1_2_1_1_yes") > -1 || window.location.href.indexOf("Q1_2_1_1_no") > -1){
-return(<li><Link to="/"><Button >Start Again</Button></Link></li>);
-  }
-
-  else{
-    return(
-<div/>
-);
-}
-}
 traverser(){
 
   if(window.location.pathname == '/'){
@@ -213,7 +100,7 @@ traverser(){
 
   )
 }
-if(window.location.pathname == '/Q1_1/Q1_2'){
+else if(window.location.pathname == '/Q1_2'){
   return(
   <h1 className ="title"><a className = "link" href="/">
   » Digital Copyright Wizard <span className="tooltiptext">Previous Question: Confused?
@@ -226,7 +113,7 @@ if(window.location.pathname == '/Q1_1/Q1_2'){
 
 )
 }
- if(window.location.pathname == '/Q1_1/Q1_2_1'){
+ else if(window.location.pathname == '/Q1_2_1'){
   return(
   <h1 className ="title"><a className = "link" href="/">
   » Digital Copyright Wizard <span className="tooltiptext">Previous Question: Confused?
@@ -241,6 +128,7 @@ if(window.location.pathname == '/Q1_1/Q1_2'){
 
 )
 }
+
 else if(window.location.pathname == '/Q1_2_1_1'){
   return(
   <h1 className ="title"><a className = "link" href="/">
@@ -398,12 +286,38 @@ else if(window.location.pathname == '/Q1_2_2_no'){
 )
 }
 }
+question_show(q){
+  var i, item, ref = {}, counts = {};
+  function ul() {return document.createElement('ul');}
+  function li(text) {
+      var e = document.createElement('li');
+      e.appendChild(document.createTextNode(text));
+      return e;
+  }
+  ref[0] = ul();
+  counts[0] = 1;
+  for (i = 0; i < q.numoptions; ++i) {
+     ref[q.optiontype[i].nextstepcontent].appendChild(li(q.optiontype[i]['option']));
+  }
+}
+question_counter = (q) =>{
+
+
+    var i = 0;
+    for (i = 0; i < q.numoptions; i++){
+      return             <div>
+            </div>
+    }
+
+
+}
+
     render() {
 
       var chosen = {
           display: this.state.chosen ? "block" : "none"
         };
-//fetch(`${process.env.PUBLIC_URL}/test.yml`)
+
     return (
 <Route>
 <div >
@@ -421,49 +335,108 @@ else if(window.location.pathname == '/Q1_2_2_no'){
          </ul>
          {this.traverser()}
          <div className="content">
-         <Route exact path="/Q1_1" component={Start}/>
+
 
 
          </div>
-       </div>
-{/*
-{this.select_question()}
-{this.traverser()}
-<h2>q1state: {this.state.qwe}</h2>
 
-<div className="Restart">
-<Button onClick={this.reset} className="bg-success" >Restart</Button>
-</div>
-    <Switch>
- <Route exact path='/' component={Home}/>
-    <Route path='/Q1_1' component={Start}/>
-    <Route path='/Q1_2' component={Q1_2}/>
-    <Route path='/Q1_2_1' component={Q1_2_1}/>
-    <Route path='/Done1' component={Done1}/>
-    <Route path='/Q1_1_2' component={Q1_1_2}/>
-    <Route path='/Q1_1_2_yes' component={Q1_1_2_yes}/>
-    <Route path='/Q1_1_2_no' component={Q1_1_2_no}/>
-    <Route path='/Q1_2_2' component={Q1_2_2}/>
-    <Route path='/Q1_2_2_yes' component={Q1_2_2_yes}/>
-    <Route path='/Q1_2_2_no' component={Q1_2_2_no}/>
-    <Route path='/Q1_2_1_1' component={Q1_2_1_1}/>
-    <Route path='/Q1_2_1_1_yes' component={Q1_2_1_1_yes}/>
-    <Route path='/Q1_2_1_1_no' component={Q1_2_1_1_no}/>
+         {
+         data.map((q) =>  {
+           while(q.questionid == window.location.pathname){
+            if(q.questionid.indexOf('done') >= 0){
+              return<div>
+              <h1>printing from json:{q.finished}</h1>
+              <div className="navigationButtonsLeft">
+              <NavLink to={q.questionorigin}><Button >&lt; Back</Button></NavLink>
 
-    </Switch>
-    <h2>Options</h2>
-{this.Q1_1check()}
-{this.Q1_2check()}
-{this.Q1_2_1check()}
-{this.Done1check()}
-{this.finalcheck1()}
-{this.Q1_1_2check()}
-{this.Q1_1_2_yesnocheck()}
-{this.Q1_2_2check()}
-{this.Q1_2_2_yesnocheck()}
-{this.Q1_2_1_1check()}
-{this.Q1_2_1_1yesnocheck()}
+                 </div>
+              </div>
+            }
+            else{
+              if(q.numoptions == 2){
+             return<div>
+
+             <h1>Printing out from Json: {q.question}</h1>
+             <div className="navigationButtonsLeft">
+             <NavLink to={q.questionorigin}><Button >&lt; Back</Button></NavLink>
+
+                </div>
+             <Button onClick={this.toggle} >  <div className = "buttondiv">More Info</div></Button>
+    <p>  {q.explanationresources}</p>
+             <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+               <ModalHeader toggle={this.toggle}>  Information </ModalHeader>
+               <ModalBody>
+            Print from json: {q.explanation}
+               </ModalBody>
+               <ModalFooter>
+                 <Button color="primary" onClick={this.toggle}>Got it!</Button>{' '}
+                 <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+               </ModalFooter>
+             </Modal>
+             <ul className="header">
+{/*<p> function:{this.question_show(q)}</p>*/}
+{/*function above work in progress*/}
+
+{/*}<p>is this here?: {Object.keys(q.optiontype).length}</p>
+<p>is this here?: {q.optiontype[0].nextstepcontent}</p>
 */}
+          <li><NavLink to={q.optionid1.nextstepcontent}><Button style={{background: this.chosen_color(0)}} onClick={() => {this.button_chosen(0)}} >{q.optionid1.option}</Button></NavLink></li>
+
+          <li><NavLink to={q.optionid2.nextstepcontent}><Button style={{background: this.chosen_color(1)}} onClick={() => {this.button_chosen(1)}} >{q.optionid2.option}</Button></NavLink></li>
+
+
+          </ul>
+        </div>
+      }
+        else if(q.numoptions == 3){
+
+          return<div>
+
+          <h1>Printing out from Json: {q.question}</h1>
+          <div className="navigationButtonsLeft">
+          <NavLink to={q.questionorigin}><Button >&lt; Back</Button></NavLink>
+
+             </div>
+          <Button onClick={this.toggle} >  <div className = "buttondiv">More Info</div></Button>
+                <p>{q.explanationresources}</p>
+          <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+            <ModalHeader toggle={this.toggle}>  Information </ModalHeader>
+            <ModalBody>
+         Print from json: {q.explanation}
+            </ModalBody>
+            <ModalFooter>
+
+              <Button color="primary" onClick={this.toggle}>Got it!</Button>{' '}
+              <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+            </ModalFooter>
+          </Modal>
+
+          <ul className="header">
+
+
+
+          <li><NavLink to={q.optionid1.nextstepcontent}><Button style={{background: this.chosen_color(0)}} onClick={() => {this.button_chosen(0)}} >{q.optionid1.option}</Button></NavLink></li>
+
+          <li><NavLink to={q.optionid2.nextstepcontent}><Button style={{background: this.chosen_color(1)}} onClick={() => {this.button_chosen(1)}} >{q.optionid2.option}</Button></NavLink></li>
+
+
+       </ul>
+     </div>
+
+        }
+        else{
+          <h1>Oops dev error </h1>
+        }
+    }
+           ;
+         }
+
+       })
+
+       }
+
+       </div>
+
 
 
 
